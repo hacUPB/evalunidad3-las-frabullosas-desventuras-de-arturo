989715d9b463a0f4c35dcf965bf548d8be8be786
ElJuanes09 <84542017+ElJuanes@users.noreply.github.com>
2022-10-26 12:55:24 -0500

    Update suicida

--- StrategyMain.c
+++ StrategyMain.c
@@ -4,23 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 int main (){
- 
 void str_ctror();    
-
 printf("Client: Strategy is set to normal sorting.");
 void strB_ctor();
 char strB_print_Arreglo();
-
 
 printf("Client: Strategy is set to normal sorting.");
 void strA_ctor();
 char strA_print_Arreglo();
 
-
-
 return EXIT_SUCCESS;
-
-
 }
